0ccf045d103686c7e42beff79221e686ea89c013
Steven Behnke <steven_behnke@me.com>
2025-02-26 21:06:41 -0600

    Update comments in main to mimic FreeRTOS examples.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+/* Example Application Tasks */
 void firstTask(void *pvParameters)
 {
     while (true) {
@@ -19,11 +20,20 @@ void secondTask(void *pvParameters)
         vTaskDelay(pdMS_TO_TICKS(950));
     }
 }
+/* End of Example Application Tasks */
+
+/* Hardware Setup */
+void prvSetupHardware(void)
+{
+    stdio_init_all();
+}
 
 int main()
 {
-    stdio_init_all();
+    /* Perform any hardware setup necessary. */
+    prvSetupHardware();
 
+    /* Example Application Tasks */
     auto result1 = xTaskCreate(firstTask, "First Task", 1024, NULL, 1, NULL);
     if (result1 != pdPASS) {
         printf("Failed to create task\n");
@@ -35,9 +45,12 @@ int main()
         printf("Failed to create task\n");
         return 1;
     }
+    /* End of Example Application Tasks */
 
-    /* Start the tasks and timer running. */
+    /* Start the created tasks running. */
     vTaskStartScheduler(); // Does not return
 
+    /* Execution will only reach here if there was insufficient heap to start the scheduler. */
+    for (;;);
     return 0;
 }
