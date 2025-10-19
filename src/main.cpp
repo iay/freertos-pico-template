995dece2d8e70f32592ba249416f281ded2f8f87
Ian Young <ian@iay.org.uk>
2025-10-19 11:23:22 +0100

    Add some task diagnostics

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,50 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+void printThisTask() {
+    TaskStatus_t task;
+    vTaskGetInfo(NULL, &task, pdTRUE, eInvalid);
+    printf("From task '%s' core %d affinity %08x", task.pcTaskName,
+      get_core_num(), task.uxCoreAffinityMask);
+    printf(" base priority %d HWM %d\n", task.uxBasePriority, task.usStackHighWaterMark);
+}
+
+void printTasks() {
+    const int max_tasks = 20;
+    static TaskStatus_t tasks[max_tasks];
+    UBaseType_t task_count = uxTaskGetSystemState(
+      tasks,
+      max_tasks,
+      NULL
+    );
+    printf("Task list, count: %d\n", task_count);
+    for (UBaseType_t t = 0; t < task_count; t++) {
+      printf("%d: '%s' affinity %08x priority %d\n", t,
+        tasks[t].pcTaskName, tasks[t].uxCoreAffinityMask,
+        tasks[t].uxBasePriority);
+      printf("   stack base %08x high water %d\n", tasks[t].pxStackBase,
+        tasks[t].usStackHighWaterMark);
+    }
+
+}
+
 /* Example Application Tasks */
 void firstTask(void *pvParameters)
 {
+    vTaskCoreAffinitySet(NULL, 1 << 0);
     while (true) {
-        printf("First task\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        printThisTask();
+        printTasks();
+        vTaskDelay(pdMS_TO_TICKS(10000));
     }
 }
 
 void secondTask(void *pvParameters)
 {
-    while (true) {
-        printf("Second task\n");
+    vTaskCoreAffinitySet(NULL, 1 << 1);
+    while (true)
+    {
+        printThisTask();
         vTaskDelay(pdMS_TO_TICKS(950));
     }
 }
